e91b4921f6e7cba0e753ba393c3481b56b44a671
maxim <admtrigger@gmail.com>
2020-03-29 18:50:18 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import uuid from 'uuid';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <>
+      <TodoInput></TodoInput>
+      <TodoList></TodoList>
+    </>
+  );
+}
+
+export default App;
