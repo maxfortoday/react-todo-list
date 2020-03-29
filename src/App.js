ab261fa5e674e3d6241ed0ab2a99536495f8e728
maxim <admtrigger@gmail.com>
2020-03-29 19:42:33 +0300

    App component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import uuid from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 
-function App() {
-  return (
-    <>
-      <TodoInput></TodoInput>
-      <TodoList></TodoList>
-    </>
-  );
+// uuidv4(); 
+
+class App extends Component {
+  state = {
+    items: [
+      {id: 1, title: 'wake up'},
+      {id: 2, title: 'make brackfast'}
+    ],
+    id: uuidv4(),
+    item: '',
+    editItem: false
+  };
+  handleChange = (e) => {console.log('handle change')}
+  handleSubmit = (e) => {console.log('handle Submit')}
+  clearList = () => {console.log('clear list')}
+  handleDelete = (id) => {console.log(`handle delete ${id}`)}
+  handleEdit = (id) => {console.log(`handle edit ${id}`)}
+  render() {    
+    return (
+        <div className="container">
+          <div className="row">
+            <div className="col-10 mx-auto col-md-8 mt 5">
+              <h3 className="text-capitalize text-center">Todo Input</h3>
+              <TodoInput 
+                item={this.state.item}
+                handleChange={this.handleChange}
+                handleSubmit={this.handleSubmit}
+                editItem={this.editItem}
+              />
+              <TodoList 
+                items={this.state.items}
+                clearList={this.clearList}
+                handleDelete={this.handleDelete}
+                handleEdit={this.handleEdit}
+              />  
+            </div>
+          </div>
+        </div>   
+    );
+  }
 }
 
 export default App;
