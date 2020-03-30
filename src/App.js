7695d78cedb3a966c705aadf13b142a8acb62a9b
maxim <admtrigger@gmail.com>
2020-03-30 12:40:15 +0300

    new things

--- src/App.js
+++ src/App.js
@@ -9,25 +9,41 @@ import TodoList from './components/TodoList';
 
 class App extends Component {
   state = {
-    items: [
-      {id: 1, title: 'wake up'},
-      {id: 2, title: 'make brackfast'}
-    ],
+    items: [],
     id: uuidv4(),
     item: '',
     editItem: false
   };
-  handleChange = (e) => {console.log('handle change')}
-  handleSubmit = (e) => {console.log('handle Submit')}
+  handleChange = (e) => {
+    this.setState({
+      item: e.target.value
+    })
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const newItem = {
+      id: this.state.id,
+      title: this.state.item
+    }
+
+    const updatedItems = [...this.state.items,newItem];
+
+    this.setState({
+      items: updatedItems,
+      item: '',
+      id:uuidv4(),
+      editItem: false
+    },console.log(this.state))
+  }
   clearList = () => {console.log('clear list')}
   handleDelete = (id) => {console.log(`handle delete ${id}`)}
   handleEdit = (id) => {console.log(`handle edit ${id}`)}
-  render() {    
+  render() {        
     return (
         <div className="container">
           <div className="row">
             <div className="col-10 mx-auto col-md-8 mt 5">
-              <h3 className="text-capitalize text-center">Todo Input</h3>
+              <h3 className="text-capitalize text-center mt-5">Todo Input</h3>
               <TodoInput 
                 item={this.state.item}
                 handleChange={this.handleChange}
